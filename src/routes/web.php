712fb0e57fff18162410ebfee9fa52ee9a1e67b7
Francesco Passanante <francescopassanante@gmail.com>
2023-09-26 17:31:23 +0200

    ref models relationship

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,12 +6,7 @@ use Fieroo\Stands\Controllers\StandsTypesController;
 Route::group(['prefix' => 'admin', 'middleware' => ['web','auth']], function() {
     Route::resource('/stands-types', StandsTypesController::class);
     Route::group(['prefix' => 'stands'], function() {
-        Route::get('/', [StandController::class, 'index']);
         Route::post('/getSelectList', [StandController::class, 'getSelectList']);
-        Route::post('/getFurnishingsList', [StandController::class, 'getFurnishingsList']);
-        Route::get('/{code_module}/{stand_type_id}/furnishings', [StandController::class, 'indexFurnishings']);
-        Route::get('/{code_module}/{stand_type_id}/furnishings/show', [StandController::class, 'showFurnishings']);
         Route::post('/getData', [StandController::class, 'getData']);
-        Route::post('/confirm', [StandController::class, 'confirm']);
     });
 });
