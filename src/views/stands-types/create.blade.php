b8345f34272cf86f36e5bd647705d72d1cfe38d6
Francesco Passanante <francescopassanante@gmail.com>
2024-07-25 17:45:12 +0200

    fix

--- src/views/stands-types/create.blade.php
+++ src/views/stands-types/create.blade.php
@@ -144,12 +144,16 @@
 @endsection
 
 @section('vendor-style')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-select/bootstrap-select.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/quill/typography.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/quill/katex.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/quill/editor.css') }}" />
 @endsection
 
 @section('vendor-script')
+    <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/bootstrap-select/bootstrap-select.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/quill/katex.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/quill/quill.js') }}"></script>
 @endsection
@@ -232,5 +236,9 @@
             const correspondingSelect = document.querySelector(`select[name="${name}_en"]`);
             correspondingSelect.value = this.value;
         }
+
+        $(document).ready(function() {
+            $('#category_id').select2();
+        })
     </script>
 @endsection
